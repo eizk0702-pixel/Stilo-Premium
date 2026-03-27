d5bc7c4e948b1b12299ca8fda01333fed2a8bb08
eizk0702-pixel <eizk0702@gmail.com>
2026-03-27 18:04:24 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyD9zO7KYM7ALgLnzgdZr3IiiJyu1M-aYWk",
   authDomain: "stilo-premium.firebaseapp.com",
   projectId: "stilo-premium",
   storageBucket: "stilo-premium.firebasestorage.app",
   messagingSenderId: "998169786088",
-  appId: "1:998169786088:web:1764eacd99eeb926547b5d",
-  measurementId: "G-7NFZ9MDJ70"
+  appId: "1:998169786088:web:435a0724bcd5bda9547b5d"
 };
+
+export const TIMEZONE = "America/Sao_Paulo";
+export const SLOT_GRANULARITY_MINUTES = 5;
+export const DEFAULT_WHATSAPP_NUMBER = "5511996511471";
